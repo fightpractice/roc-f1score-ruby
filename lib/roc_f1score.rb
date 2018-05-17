832723d40c48201c827e8b95c6abff2bd01cd375
hehui <yystopf@163.com>
2018-05-17 10:56:04 +0800

    finish

--- lib/roc_f1score.rb
+++ lib/roc_f1score.rb
@@ -1,4 +1,27 @@
 class RocF1score
   # author vietlami
 
+  def self.f1score(actual,predict)
+    tp = 0
+    fp = 0
+    fn = 0
+    indexes = actual.each_index.collect{|index| index}
+    indexes.each do |i|
+      if actual[i] == 1
+        if actual[i] == predict[i]
+          tp += 1
+        else
+          fn += 1
+        end
+      end
+      if actual[i] == 0
+        if actual[i] == predict[i]
+          tp += 1
+        else
+          fp += 1
+        end
+      end
+    end
+    return score = 2*tp/(2*tp+fn+fp)
+  end
 end
